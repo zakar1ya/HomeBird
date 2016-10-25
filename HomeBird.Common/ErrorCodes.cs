04c821dd1ed016dcb43fff1bab2611d2631fb8d1
S_Konovalov <sergei.konovalow2012@yandex.ru>
2016-10-25 22:50:18 +0300

    error codes

---
+++ HomeBird.Common/ErrorCodes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBird.Common
+{
+    public static class ErrorCodes
+    {
+        private static Dictionary<int, string> _messages;
+
+        public const int Ok = 0;
+        public const int LotNotFound = 1;
+        public const int IncubatorNotFound = 2;
+        public const int IncubatorAlreadyExist = 1;
+
+        static ErrorCodes()
+        {
+            _messages = new Dictionary<int, string>();
+
+            _messages.Add(LotNotFound, "Партия не найдена");
+            _messages.Add(IncubatorNotFound, "Инкубатор не найден");
+            _messages.Add(IncubatorAlreadyExist, "Инкубатор с таким именем уже существует");
+        }
+    }
+}
---
+++ HomeBird.Common/HbResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBird.Common
+{
+    public class HbResult<T>
+    {
+        public HbResult(T result)
+        {
+            Result = result;
+        }
+
+        public HbResult(int errorCode)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public int ErrorCode { get; set; }
+
+        public bool IsCorrect
+        {
+            get
+            {
+                return ErrorCode == 0;
+            }
+        }
+
+        public T Result { get; set; }
+    }
+}
