95c3a4967902ee822977d31e2810b3319ae3bccf
S_Konovalov <sergei.konovalow2012@yandex.ru>
2016-11-04 23:16:42 +0300

    update mappings

--- HomeBird.DataBase.Logic/Mapping/MapperProfile.cs
+++ HomeBird.DataBase.Logic/Mapping/MapperProfile.cs
@@ -24,13 +24,20 @@ namespace HomeBird.DataBase.Logic.Mapping
                 .ForMember(u => u.SoldCount, opt => opt.MapFrom(u => u.SoldCount))
                 .ForAllOtherMembers(u => u.Ignore());
 
+            CreateMap<HbLot, UpdateLotForm>()
+                .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
+                .ForMember(u => u.IdentifierNumber, opt => opt.MapFrom(u => u.IdentifierNumber))
+                .ForAllOtherMembers(u => u.Ignore());
+
             CreateMap<HbIncubators, HbIncubator>()
                 .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
-                .ForMember(u => u.Title, opt => opt.MapFrom(u => u.Title));
+                .ForMember(u => u.Title, opt => opt.MapFrom(u => u.Title))
+                .ForAllOtherMembers(u => u.Ignore());
 
             CreateMap<HbIncubator, UpdateIncubatorForm>()
                 .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
-                .ForMember(u => u.Title, opt => opt.MapFrom(u => u.Title));
+                .ForMember(u => u.Title, opt => opt.MapFrom(u => u.Title))
+                .ForAllOtherMembers(u => u.Ignore());
 
             CreateMap<HbPurchases, HbPurchase>()
                 .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
@@ -38,14 +45,33 @@ namespace HomeBird.DataBase.Logic.Mapping
                 .ForMember(u => u.Amount, opt => opt.MapFrom(u => u.Amount))
                 .ForMember(u => u.Count, opt => opt.MapFrom(u => u.Count))
                 .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
-                .ForMember(u => u.PurchaseDate, opt => opt.MapFrom(u => u.PurchaseDate));
+                .ForMember(u => u.PurchaseDate, opt => opt.MapFrom(u => u.PurchaseDate))
+                .ForAllOtherMembers(u => u.Ignore());
+
+            CreateMap<HbPurchase, UpdatePurchaseForm>()
+                .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
+                .ForMember(u => u.Address, opt => opt.MapFrom(u => u.Address))
+                .ForMember(u => u.Amount, opt => opt.MapFrom(u => u.Amount))
+                .ForMember(u => u.Count, opt => opt.MapFrom(u => u.Count))
+                .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
+                .ForMember(u => u.PurchaseDate, opt => opt.MapFrom(u => u.PurchaseDate))
+                .ForAllOtherMembers(u => u.Ignore());
 
             CreateMap<HbOverheads, HbOverhead>()
                 .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
                 .ForMember(u => u.Amount, opt => opt.MapFrom(u => u.Amount))
                 .ForMember(u => u.Comment, opt => opt.MapFrom(u => u.Comment))
                 .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
-                .ForMember(u => u.OverheadDate, opt => opt.MapFrom(u => u.OverheadDate));
+                .ForMember(u => u.OverheadDate, opt => opt.MapFrom(u => u.OverheadDate))
+                .ForAllOtherMembers(u => u.Ignore());
+
+            CreateMap<HbOverhead, UpdateOverheadsForm>()
+                .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
+                .ForMember(u => u.Amount, opt => opt.MapFrom(u => u.Amount))
+                .ForMember(u => u.Comment, opt => opt.MapFrom(u => u.Comment))
+                .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
+                .ForMember(u => u.OverheadDate, opt => opt.MapFrom(u => u.OverheadDate))
+                .ForAllOtherMembers(u => u.Ignore());
 
             CreateMap<HbSales, HbSale>()
                 .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
@@ -55,7 +81,19 @@ namespace HomeBird.DataBase.Logic.Mapping
                 .ForMember(u => u.Buyer, opt => opt.MapFrom(u => u.Buyer))
                 .ForMember(u => u.Comment, opt => opt.MapFrom(u => u.Comment))
                 .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
-                .ForMember(u => u.Type, opt => opt.MapFrom(u => u.Type));
+                .ForMember(u => u.Type, opt => opt.MapFrom(u => u.Type))
+                .ForAllOtherMembers(u => u.Ignore());
+
+            CreateMap<HbSale, UpdateSaleForm>()
+                .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
+                .ForMember(u => u.Amount, opt => opt.MapFrom(u => u.Amount))
+                .ForMember(u => u.SaleDate, opt => opt.MapFrom(u => u.SaleDate))
+                .ForMember(u => u.Count, opt => opt.MapFrom(u => u.Count))
+                .ForMember(u => u.Buyer, opt => opt.MapFrom(u => u.Buyer))
+                .ForMember(u => u.Comment, opt => opt.MapFrom(u => u.Comment))
+                .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
+                .ForMember(u => u.Type, opt => opt.MapFrom(u => u.Type))
+                .ForAllOtherMembers(u => u.Ignore());
 
             CreateMap<HbLayings, HbLaying>()
                 .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
@@ -63,7 +101,16 @@ namespace HomeBird.DataBase.Logic.Mapping
                 .ForMember(u => u.CreationDate, opt => opt.MapFrom(u => u.CreationTime))
                 .ForMember(u => u.EggPrice, opt => opt.MapFrom(u => u.EggPrice))
                 .ForMember(u => u.IncubatorId, opt => opt.MapFrom(u => u.IncubatorId))
-                .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId));
+                .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
+                .ForAllOtherMembers(u => u.Ignore());
+
+            CreateMap<HbLaying, UpdateLayingForm>()
+                .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
+                .ForMember(u => u.Count, opt => opt.MapFrom(u => u.Count))
+                .ForMember(u => u.CreationDate, opt => opt.MapFrom(u => u.CreationDate))
+                .ForMember(u => u.IncubatorId, opt => opt.MapFrom(u => u.IncubatorId))
+                .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
+                .ForAllOtherMembers(u => u.Ignore());
 
             CreateMap<HbBroods, HbBrood>()
                 .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
@@ -75,7 +122,17 @@ namespace HomeBird.DataBase.Logic.Mapping
                 .ForMember(u => u.EmptyPercent, opt => opt.MapFrom(u => u.EmptyPercent))
                 .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
                 .ForMember(u => u.Percent, opt => opt.MapFrom(u => u.Percent))
-                .ForMember(u => u.PlacePrice, opt => opt.MapFrom(u => u.PlacePrice));
+                .ForMember(u => u.PlacePrice, opt => opt.MapFrom(u => u.PlacePrice))
+                .ForAllOtherMembers(u => u.Ignore());
+
+            CreateMap<HbBroods, UpdateBroodForm>()
+                .ForMember(u => u.Id, opt => opt.MapFrom(u => u.Id))
+                .ForMember(u => u.Count, opt => opt.MapFrom(u => u.Count))
+                .ForMember(u => u.BroodDate, opt => opt.MapFrom(u => u.BroodDate))
+                .ForMember(u => u.DeadCount, opt => opt.MapFrom(u => u.DeadCount))
+                .ForMember(u => u.EmptyCount, opt => opt.MapFrom(u => u.EmptyCount))
+                .ForMember(u => u.LotId, opt => opt.MapFrom(u => u.LotId))
+                .ForAllOtherMembers(u => u.Ignore());
         }
     }
 }
